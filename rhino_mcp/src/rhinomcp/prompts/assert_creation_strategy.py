9a21e2ca5fa47caf3930b83c5e41c2bd6d2a8127
Jingcheng Chen <mail@jchen.ch>
2025-03-24 22:30:44 +0100

    added box hack for the code execution, should move to resources

--- rhino_mcp/src/rhinomcp/prompts/assert_creation_strategy.py
+++ rhino_mcp/src/rhinomcp/prompts/assert_creation_strategy.py
@@ -8,9 +8,13 @@ def asset_creation_strategy() -> str:
 
     0. Before anything, always check the document from get_document_info()
     1. Please ALWAYS try to create the objects using the tool execute_rhinoscript_python_code() first.
-    2. When creating rhinoscript python code: do not hallucinate, only use the syntax that is supported by rhinoscriptsyntax or Rhino,Geometry.
-    3. If there are multiple objects, use the method create_objects() to create multiple objects at once. Do not attempt to create them one by one if they are more than 10.
-    4. When including an object into document, ALWAYS make sure that the name of the object is meanful.
-    5. Try to include as many objects as possible accurately and efficiently. If the command is not able to include
+    2. If there are multiple objects, use the method create_objects() to create multiple objects at once. Do not attempt to create them one by one if they are more than 10.
+    3. When including an object into document, ALWAYS make sure that the name of the object is meanful.
+    4. Try to include as many objects as possible accurately and efficiently. If the command is not able to include
        so many data, try to create the objects in batches.
+
+    When creating rhinoscript python code:
+    - do not hallucinate, only use the syntax that is supported by rhinoscriptsyntax or Rhino,Geometry.
+    - document the code that you are writing.
+    - double check the code if any of the code is not correct, and fix it.
     """
--- rhino_mcp/src/rhinomcp/tools/execute_rhinoscript_python_code.py
+++ rhino_mcp/src/rhinomcp/tools/execute_rhinoscript_python_code.py
@@ -13,6 +13,20 @@ def execute_rhinoscript_python_code(ctx: Context, code: str) -> str:
     
     Parameters:
     - code: The RhinoScript code to execute
+
+    References:
+    AddBox(corners)
+        Adds a box shaped polysurface to the document
+    Parameters:
+        corners ([point, point, point ,point, point, point ,point,point]) 8 points that define the corners of the box. Points need to
+        be in counter-clockwise order starting with the bottom rectangle of the box
+    Returns:
+        guid: identifier of the new object on success
+    Example:
+        import rhinoscriptsyntax as rs
+        box = rs.GetBox()
+        if box: rs.AddBox(box)
+
     """
     try:
         # Get the global connection
